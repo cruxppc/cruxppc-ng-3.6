cb895399ebada923181d210738db25d08fc09b5c
cruxppc <cruxppc@gmail.com>
2021-03-10 14:37:24 +0000

    Update post install script.

--- misc/post-installs/POST.sh
+++ misc/post-installs/POST.sh
@@ -8,5 +8,6 @@
 (pkginfo -i |grep '^libglade ')  &&  (/bin/bash -v libglade/post-install)
 (pkginfo -i |grep '^libinput ')  &&  (/bin/bash -v libinput/post-install)
 (pkginfo -i |grep '^librsvg ')  &&  (/bin/bash -v librsvg/post-install)
-(pkginfo -i |grep '^shared-mime-info ')  &&  (/bin/bash -v shared-mime-info/post-install)
+(pkginfo -i |grep '^pcmanfm ')  &&  (/bin/bash -v pcmanfm/post-install)
+(pkginfo -i |grep '^pulseaudio ')  &&  (/bin/bash -v pulseaudio/post-install)
 (pkginfo -i |grep '^xorg-font-misc-misc ')  &&  (/bin/bash -v xorg-font-misc-misc/post-install)
